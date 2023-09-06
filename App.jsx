0dacd9b65dfdd5ea9b7a0de86454aac0313814c8
barockatr <137476226+barockatr@users.noreply.github.com>
2023-09-06 15:24:21 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+import Cards from './components/cards/Cards.jsx';
+import Nav from './components/nav/Nav';
+import { useState } from 'react';
+import axios from "axios";
+
+function App() {
+   const example = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      origin: {
+         name: 'Earth (C-137)',
+         url: 'https://rickandmortyapi.com/api/location/1',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+   };
+
+   const [ characters, setCharacters ] = useState([]); // [ estado, functión ]
+   
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      setCharacters(characters.filter(char => char.id !== Number(id)))
+   }
+   // characters = [ {id:1}, {id:3} ]
+   // id = 2
+
+   return (
+      <div className='App'>
+         <Nav onSearch={onSearch} />
+         <Cards characters={characters} onClose={onClose} />
+      </div>
+   );
+}
+
+export default App;
